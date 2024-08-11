ba0732ab3452eee6a1ac295aace599752e4fab8b
MG <you@example.com>
2024-08-11 03:01:21 +0300

    Bearer JWT token setup

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
+using System.Text;
 using dotnet_ids.Repository;
 using dotnet_ids.Repository.IRepository;
 using Dotnetids.Data;
-using Dotnetids.Models;
+using Dotnetids.Models.Entity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Solution.dotnet_ids.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,6 +28,30 @@ builder.Services.AddScoped<IRepository<Member>, MemberRepository>();
 builder.Services.AddScoped<IRepository<Guide>, GuideRepository>();
 builder.Services.AddScoped<IRepository<Event>, EventRepository>();
 
+
+
+//> Bearer Auth
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secret") ?? throw new InvalidOperationException("The JWT secret key cannot be null.");
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }
+).AddJwtBearer(
+    options =>
+    {
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    }
+);
 
 var app = builder.Build();
 
